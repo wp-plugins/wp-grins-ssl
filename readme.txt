41f8bf40a7084b7dcd1f5ad176625b5841c8060c
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-19 02:13:20 +0000

    Finally sorted a workaround for bbp 2.1. If you're using _bbp_use_wp_editor, then you don't need this. I think it works.
    git-svn-id: https://plugins.svn.wordpress.org/wp-grins-ssl/tags/4.3@560232 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,98 @@
+=== WP Grins SSL ===
+Tags: clickable, smilies, comments, bbpress, wpgrins
+Contributors: alexkingorg, ronalfy, Ipstenu
+Requires at least: 3.0
+Tested up to: 3.4
+Stable tag: 4.3
+Donate link: https://www.wepay.com/donations/halfelf-wp
+License: GPL2
+
+WP Grins SSL provides smilies for your comment areas and is compatible with SSL Administration.
+
+== Description ==
+
+WP Grins SSL will provide clickable smilies for both the post form in the admin interface and the comments form of your blog.  WP Grins SSL is a fork of WP Grins that allows for use on a site using [Administration over SSL](http://codex.wordpress.org/Administration_Over_SSL) or not, auto-detecting which is why and displaying either way.
+
+As of version 4.0, WP Grins SSL also works on bbPress (the plugin) and has an option to turn it on or off as desired.
+
+Tested on Single Site and MultiSite.
+
+* [Plugin Site](http://halfelf.org/plugins/wp-grins-ssl/)
+* [Donate](https://www.wepay.com/donations/halfelf-wp)
+
+== Installation ==
+
+1. Install and activate the plugin.
+2. Visit your <em>Discussion Settings</em> page.
+3. Look for 'Smilies' and check boxes as desired. (Comments are activated by default)
+
+== Changelog ==
+
+= 4.3 = 
+* 2012 June 18, by Ipstenu
+* bbPress 2.1's Fancy Editor borks this.
+
+= 4.2.1 =
+* 2012 June 02, by Ipstenu
+* Typos. A-freaking-gain
+
+= 4.2 =
+* 2012 June 02, by Ipstenu
+* bbPress refining (testing with 2.1 as well since that's a thing)
+
+= 4.1 =
+* 2012 May 07, by Ipstenu
+* bbPress is an option.
+
+= 4.0 =
+* 27 April 2012 by Ipstenu
+* Major bump. Why? Oh this little thing I like to call bbPress. Fully supported.
+
+= 3.1 =
+* 17 April 2012 by Ipstenu
+* Trying to fix why it decides, magically, not to show up. Sometimes. Should work. Works on 3.4...
+
+= 3.0 =
+* 22 February 2012 by Ipstenu
+* Reverted back to Alex's use of Prototype because something went funky cold medina with ajax.
+
+= 2.2 =
+* 1 October 2011 by Ipstenu
+* Minification for faster everythinging.
+
+= 2.1 =
+* 22 September 2011 by Ipstenu
+* Removing PHP 4 support (WP doesn't).
+* Cleaning out unused aspects from removal of admin panel.
+
+= 2.0 =
+* 21 September 2011 by Ipstenu
+* Fixed issue with SSL adminstration causing the icons not to show up.
+* Removed admin panel. It's 2011. If your theme doesn't have wp_head, you have other issues.
+* Consolidated stylesheets (dropping support for IE 6).
+* Renamed files and pulled into one directory.
+* Compressed files.
+
+= 1.1 = 
+* 04 November 2009 by Ronalfy
+* Fixed bug where grins would show up in the comments panel.
+* Re-did the JavaScript to it's completely separate and only runs when necessary.
+* Added admin panel option to manual insert smilies on a page or a post.
+
+= 1.0 = 
+* 25 October 2009 by Ronalfy
+* First release.  Yay!
+
+== Frequently Asked Questions ==
+
+= Why don't the smilies show up in my comments form? =
+Your theme must include the `wp_head` call and the comments field in your theme must have an id of `comment`.  This version of the plugin does not support manual insertion, because, frankly, no decent WordPress theme is lacking `wp_head` anymore.
+
+= Why did this start with version 2? =
+Because the previous fork-source was 1.0, and this is really just an extension of all that work.  Wanted to keep Ronafly's credits up in there!
+
+= Will you support BuddyPress? =
+Eventually. I'm not running it right now, so it's hard to test.
+
+= Why isn't this working on bbPress? =
+Do you have the fancy editor? Yeah it doesn't work. Sorry, I don't know why. This is a 'fast' fix. If you have the bbPress Fancy Editor on, this plugin goes away for bbPress.
---
+++ wp-grins.php
@@ -0,0 +1,203 @@
+<?php
+
+// WP Grins SSL
+//
+// Original plugin WP Grins
+// Copyright (c) 2004-2007 Alex King
+// http://alexking.org/projects/wordpress
+//
+// SSL version created on June 20, 2008 by Ronald Huereca
+// SSL version created on Sept 21, 2011 by Mika "Ipstenu" Epstein
+// Copyright 20011 Mika Epstein (email: ipstenu@ipstenu.org)
+//
+// This is an add-on for WordPress
+// http://wordpress.org/
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// *****************************************************************
+
+/*
+Plugin Name: WP Grins SSL
+Plugin URI: http://halfelf.org/plugins/wp-grins-ssl
+Description: A Clickable Smilies hack for WordPress.
+Version: 4.3
+Author: Alex King, Ronald Huereca, Mika Epstein
+Author URI: http://www.ipstenu.org
+Props:  Original author, Alex King.  Original fork, Ronald Huereca
+*/
+
+if (!class_exists('WPGrins')) {
+    class WPGrins	{
+		/**
+		* PHP 5 Constructor
+		*/		
+		function __construct(){
+			//Scripts
+			add_action('wp_print_scripts', array(&$this,'add_scripts_frontend'),1000);
+			//Styles
+			add_action('wp_print_styles', array(&$this,'add_styles_frontend'));
+			
+			//Ajax
+			add_action('wp_ajax_grins', array(&$this,'ajax_print_grins'));
+			add_action('wp_ajax_nopriv_grins', array(&$this,'ajax_print_grins')); 
+		}
+		function ajax_print_grins() {
+			echo $this->wp_grins();
+			exit;
+		}
+		function wp_grins() {
+				global $wpsmiliestrans;
+				$grins = '';
+				$smiled = array();
+				foreach ($wpsmiliestrans as $tag => $grin) {
+					if (!in_array($grin, $smiled)) {
+						$smiled[] = $grin;
+						$tag = esc_attr(str_replace(' ', '', $tag));
+						$src = esc_url(site_url("wp-includes/images/smilies/{$grin}", "http"));
+						$grins .= "<img src='$src' alt='$tag' onclick='jQuery.wpgrins.grin(\"$tag\");' />";
+					}
+				}
+				return $grins;
+		} //end function wp_grins
+		
+		function add_styles() {
+			wp_enqueue_style('wp-grins', plugins_url('wp-grins-ssl/wp-grins.css'));
+		}
+		function add_scripts() {
+			wp_enqueue_script('wp_grins_ssl', plugins_url('wp-grins-ssl/wp-grins.js'), array("jquery"), 1.0); 
+			wp_localize_script( 'wp_grins_ssl', 'wpgrinsssl', $this->get_js_vars());
+		}
+		
+		function add_styles_frontend() {
+    		$options = get_option('ippy_wpgs_options');
+    		$valuebb = $options['bbpress'];
+    		$valueco = $options['comments'];
+    		$ippy_wpgs_bbp_fancy = get_option( '_bbp_use_wp_editor' );
+    		
+    		if ( function_exists('is_bbpress') ) {
+                if ( is_bbpress()  && ( $valuebb != '0') && !is_null($valuebb) && ($ippy_wpgs_bbp_fancy == '0') ) {
+                    $this->add_styles();
+                }
+              }
+            if ( comments_open() && is_singular() && ( $valueco != '0') && !is_null($valueco) ) {
+                $this->add_styles();
+            }
+        }		
+		function add_scripts_frontend() {
+    		$options = get_option('ippy_wpgs_options');
+    		$valuebb = $options['bbpress'];
+    		$valueco = $options['comments'];
+    		$ippy_wpgs_bbp_fancy = get_option( '_bbp_use_wp_editor' );
+    		
+    		if ( function_exists('is_bbpress') ) {
+                if ( is_bbpress()  && ( $valuebb != '0') && !is_null($valuebb) && ($ippy_wpgs_bbp_fancy == '0') ) {
+                    $this->add_scripts();
+                }
+              }
+            if ( comments_open() && is_singular() && ( $valueco != '0') && !is_null($valueco) ) {
+                $this->add_scripts();
+            }
+        }
+            //Returns various JavaScript vars needed for the scripts
+            function get_js_vars() {
+                if (is_ssl()) {
+                   	$schema_ssl = 'https'; 
+                } else { 
+                   	$schema_ssl = 'http'; 
+                }
+                return array(
+                    'Ajax_Url' => admin_url('admin-ajax.php', $schema_ssl),
+                    'LOCATION' => 'admin'
+                );
+            } //end get_js_vars
+		/*END UTILITY FUNCTIONS*/
+    }
+}
+//instantiate the class
+if (class_exists('WPGrins')) {
+	$GrinsSSL = new WPGrins();
+}
+// left in for legacy reasons
+if (!function_exists('wp_grins')) {
+	function wp_grins() { 
+		print('');
+	}
+}
+if (!function_exists('wp_print_grins')) {
+	function wp_print_grins() {
+		global $GrinsSSL;
+		if (isset($GrinsSSL)) {
+			return $GrinsSSL->wp_grins();
+		}
+	}
+}
+
+// Register and define the settings
+add_action('admin_init', 'ippy_wpgs_admin_init');
+
+function ippy_wpgs_admin_init(){
+
+	register_setting(
+		'discussion',               // settings page
+		'ippy_wpgs_options',         // option name
+		'ippy_wpgs_validate_options' // validation callback
+	);
+	
+	add_settings_field(
+		'ippy_wpgs_bbpress',         // id
+		'WP Grins',                // setting title
+		'ippy_wpgs_setting_input',   // display callback
+		'discussion',               // settings page
+		'default'                   // settings section
+	);
+}
+
+register_activation_hook( __FILE__, 'ippy_wpgs_activate' );
+
+function ippy_wpgs_activate() {
+	$options = get_option( 'ippy_wpgs_options' );
+	$options['comments'] = '0';
+	$options['bbpress'] = '0';
+	update_option('ippy_wpgs_options', $options);
+}
+
+// Display and fill the form field
+function ippy_wpgs_setting_input() {
+	// get option value from the database
+	$options = get_option( 'ippy_wpgs_options' );
+	$valuebb = $options['bbpress'];
+	$valueco = $options['comments'];
+	$ippy_wpgs_bbp_fancy = get_option( '_bbp_use_wp_editor' );
+	?>
+<p><?php 
+	if ( function_exists('is_bbpress') && ($ippy_wpgs_bbp_fancy == '0') ) { ?>
+<input id='bbpress' name='ippy_wpgs_options[bbpress]' type='checkbox' value='1' <?php if ( ( $valuebb != '0') && !is_null($valuebb) ) { echo ' checked="checked"'; } ?> /> Activate Smilies for bbPress<br /> <?php } 
+	else { ?>
+	<input type='hidden' id='bbpress' name='ippy_wpgs_options[bbpress]' value='0'> <?php } 
+?>
+<input id='comments' name='ippy_wpgs_options[comments]' type='checkbox' value='1' <?php if ( ( $valueco != '0') && !is_null($valueco) ) { echo ' checked="checked"'; } ?> /> Activate Smilies for comments
+	<?php
+}
+
+// Validate user input
+function ippy_wpgs_validate_options( $input ) {
+	$valid = array();
+	$valid['comments'] = $input['comments'];
+	$valid['bbpress'] = $input['bbpress'];
+	unset( $input );
+	return $valid;
+}
+
+// donate link on manage plugin page
+
+add_filter('plugin_row_meta', 'ippy_wpgs_donate_link', 10, 2);
+function ippy_wpgs_donate_link($links, $file) {
+        if ($file == plugin_basename(__FILE__)) {
+                $donate_link = '<a href="https://www.wepay.com/donations/halfelf-wp">Donate</a>';
+                $links[] = $donate_link;
+        }
+        return $links;
+}
