3fdae56628a12707e23742785616fc48dafad5e2
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-14 22:09:55 +0000

    Changing URL for donations
    git-svn-id: https://plugins.svn.wordpress.org/wp-grins-ssl/tags/4.3@838642 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-grins.php
+++ wp-grins.php
@@ -196,7 +196,7 @@ function ippy_wpgs_validate_options( $input ) {
 add_filter('plugin_row_meta', 'ippy_wpgs_donate_link', 10, 2);
 function ippy_wpgs_donate_link($links, $file) {
         if ($file == plugin_basename(__FILE__)) {
-                $donate_link = '<a href="https://www.wepay.com/donations/halfelf-wp">Donate</a>';
+                $donate_link = '<a href="https://store.halfelf.org/donate/">Donate</a>';
                 $links[] = $donate_link;
         }
         return $links;
